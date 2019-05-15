75c94521367f96c0c5aa9e728dd94497c13699f4
Stas Vasilev <stas@hotelchat.me>
2019-05-15 23:48:30 +0300

    quick union improved

--- union-find/QuickFind.java
+++ union-find/QuickFind.java
@@ -31,14 +31,14 @@ class QuickFindUF implements UnionFindInterface {
 
 public class QuickFind {
     public static void main(String[] args) {
-        UnionFindInterface qu = new QuickFindUF(5);
+        UnionFindInterface qf = new QuickFindUF(5);
 
-        qu.union(0, 1);
-        qu.union(3, 4);
-        qu.union(0, 3);
-        assert(qu.connected(0, 1) == true);
-        assert(qu.connected(1, 2) == false);
-        assert(qu.connected(1, 4) == true);
+        qf.union(0, 1);
+        qf.union(3, 4);
+        qf.union(0, 3);
+        assert(qf.connected(0, 1) == true);
+        assert(qf.connected(1, 2) == false);
+        assert(qf.connected(1, 4) == true);
     }
 }
 
--- union-find/QuickUnionImproved.java
+++ union-find/QuickUnionImproved.java
@@ -1,21 +1,28 @@
+// Two improvements:
+// 1. Weighted
+// 2. With path compression
+
 interface UnionFindInterface {
     void union(int p, int q);
     boolean connected(int p, int q);
 }
 
-class QuickUnionUF implements UnionFindInterface {
-
+class QuickUnionImprovedUF implements UnionFindInterface {
     private int id[];
+    private int sz[]; // number of objects
 
-    QuickUnionUF(int N) {
+    QuickUnionImprovedUF(int N) {
         id = new int[N];
+        sz = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
+            sz[i] = 1;
         }
     }
 
     private int root(int p) {
         while (p != id[p]) {
+            id[p] = id[id[p]];
             p = id[p];
         }
         return p;
@@ -24,7 +31,14 @@ class QuickUnionUF implements UnionFindInterface {
     public void union(int p, int q) {
         int rootP = root(p);
         int rootQ = root(q);
-        id[rootP] = rootQ;
+        if (sz[rootP] < sz[rootQ]) {
+            id[rootP] = rootQ;
+            sz[rootQ] += sz[rootP];
+        } else {
+            id[rootQ] = rootP;
+            sz[rootP] += sz[rootQ]; 
+        }
+        
     }
 
     public boolean connected(int p, int q) {
@@ -32,9 +46,9 @@ class QuickUnionUF implements UnionFindInterface {
     }
 }
 
-public class QuickUnion {
+public class QuickUnionImproved {
     public static void main(String[] args) {
-        UnionFindInterface qu = new QuickUnionUF(5);
+        UnionFindInterface qu = new QuickUnionImprovedUF(5);
 
         qu.union(0, 1);
         qu.union(3, 4);
