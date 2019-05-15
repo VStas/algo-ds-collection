1129580542c2d6c3e9804a70af9455b85870e6cf
Stas Vasilev <stas@hotelchat.me>
2019-05-15 22:55:02 +0300

    quick find

---
+++ union-find/QuickUnion.java
@@ -0,0 +1,43 @@
+interface QuickUnionInterface {
+    void union(int p, int q);
+    boolean connected(int p, int q);
+}
+
+class QuickFindUF implements QuickUnionInterface {
+    private int id[];
+
+    QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        int idp = id[p];
+        int idq = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == idp) {
+                id[i] = idq;
+            }
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+}
+
+public class QuickUnion {
+    public static void main(String[] args) {
+        QuickUnionInterface qu = new QuickFindUF(5);
+
+        qu.union(0, 1);
+        qu.union(3, 4);
+        qu.union(0, 3);
+        assert(qu.connected(0, 1) == true);
+        assert(qu.connected(1, 2) == false);
+        assert(qu.connected(1, 4) == true);
+    }
+}
+
